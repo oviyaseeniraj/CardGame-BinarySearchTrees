060645bb724ab90a2481039335f822860b7ec3f7
Oviya Seeniraj <oviya@engineering.ucsb.edu>
2023-03-01 14:38:23 -0800

    v1 all

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include "cards.h"
 
 using namespace std;
 
@@ -20,17 +23,97 @@ int main(int argv, char** argc){
   }
 
   //Read each file
+  
+  IntBST bstAlice;
+  IntBST bstBob;
+  int minA = 1000;
+  int maxB = -1000;
+
   while (getline (cardFile1, line) && (line.length() > 0)){
 
+    int suit;
+    int card;
+
+    stringstream ss(line);
+    string suitStr = "";
+    getline(ss, suitStr, ' ');
+    
+    string cardStr = "";
+    getline(ss, cardStr, ' ');
+    
+    if (suitStr == "c")
+      suit = 100;
+    else if (suitStr == "d")
+      suit = 200;
+    else if (suitStr == "s")
+      suit = 300;
+    else if (suitStr == "h")
+      suit == 400;
+    
+    if (cardStr == "a")
+      card = suit + 1;
+    else if (cardStr == "j")
+      card = suit + 11;
+    else if (cardStr == "q")
+      card = suit + 12;
+    else if (cardStr == "k")
+      card = suit + 13;
+    else
+      card = suit + stoi(cardStr);
+
+    bstAlice.insert(card);
+    if (card < minA)
+      minA = card;
   }
   cardFile1.close();
 
 
   while (getline (cardFile2, line) && (line.length() > 0)){
+    
+    int suit;
+    int card;
 
+    stringstream ss(line);
+    string suitStr = "";
+    getline(ss, suitStr, ' ');
+    
+    string cardStr = "";
+    getline(ss, cardStr, ' ');
+    
+    if (suitStr == "c")
+      suit = 100;
+    else if (suitStr == "d")
+      suit = 200;
+    else if (suitStr == "s")
+      suit = 300;
+    else if (suitStr == "h")
+      suit == 400;
+    
+    if (cardStr == "a")
+      card = suit + 1;
+    else if (cardStr == "j")
+      card = suit + 11;
+    else if (cardStr == "q")
+      card = suit + 12;
+    else if (cardStr == "k")
+      card = suit + 13;
+    else
+      card = suit + stoi(cardStr);
+
+    bstBob.insert(card);
+    if (card > maxB)
+        maxB = card;
   }
   cardFile2.close();
   
-  
+  while (bstAlice.playB(bstBob))
+  {
+    bstBob.playA(bstAlice);
+  }
+
+  while (bstBob.playA(bstAlice))
+  {
+    bstAlice.playB(bstBob);
+  }
   return 0;
 }
